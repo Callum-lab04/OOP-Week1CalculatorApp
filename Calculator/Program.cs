4a2590ca5df57cd36a73de0dd7f4421903d18bf2
Callum McConnell <McConnell-C28@ulster.ac.uk>
2024-09-24 15:43:17 +0100

    A simple program that adds two numbers.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Type in the first number, and then press Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number, and then press Enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
