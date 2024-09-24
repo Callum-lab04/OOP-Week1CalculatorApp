c788f6cbf90825899ac1f018200e84a3c3a0aa84
Callum McConnell <McConnell-C28@ulster.ac.uk>
2024-09-24 16:07:21 +0100

    Added code like ifs etc.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,11 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
 
-Console.WriteLine("Type in the first number, and then press Enter");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+CalculatorApp();
 
-Console.WriteLine("Type in the second number, and then press Enter");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+void CalculatorApp()
+{
+    // Declare the variables and initialise them to 0
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-int result = firstNumber + secondNumber;
+    // Ask the user to input the first number
+    Console.WriteLine("Type in the first number, and then press Enter");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
+    // Ask the user to input the second number
+    Console.WriteLine("Type in the second number, and then press Enter");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    // Use a loop to make the relevant decision
+    // and perform the request maths operation
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+
+    // Convert string to integer
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    // perform the calculation
+    result = firstNumber + secondNumber;
+
+    // Now input the answer to the console
+    Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
+}
